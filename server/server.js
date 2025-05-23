2f3fae16559f500dd64798af687795264b0baaec
sr931662 <83828392+sr931662@users.noreply.github.com>
2025-05-23 22:05:49 +0530

    init

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use('/api', limiter);
 // CORS configuration
 app.use(
   cors({
-    origin: 'https://utkarshgupta.vercel.app',
+    origin: ['http://localhost:3000', 'https://utkarshgupta.vercel.app'],
     methods: 'GET,POST,PUT,DELETE,PATCH,HEAD',
     credentials: true,
   })
